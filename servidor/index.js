4c322fce1302c3117a3820cee9db35560361b043
Marc <cuencamarc96@gmail.com>
2020-05-16 11:11:48 +0200

    Start server conf

---
+++ servidor/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const conectarDB = require("./config/db");
+// crear el servidor
+const app = express();
+
+//Conectar a la base de datos
+conectarDB();
+
+// puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// arrancar la app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+});
